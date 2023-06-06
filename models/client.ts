0f559dad21d001414e00a5fff06e1805f62adc89
Martín Gesualdo <mgesudev@gmail.com>
2023-06-06 11:33:48 -0300

    CRUD con clientes

---
+++ models/client.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose"
+
+const clientSchema = new Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  document_type: { type: String, required: true },
+  document_value: { type: String, required: true },
+  sales: {
+    type: {
+      count: Number,
+      amount: Number,
+    },
+  },
+})
+const ClientModel = model("Client", clientSchema, "clients")
+export default ClientModel
--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import authRoutes from "./auth"
 import salesRoutes from "./sales"
+import clientsRoutes from "./clients"
 const router = express.Router()
 
 router.use("/auth", authRoutes)
 router.use("/sales", salesRoutes)
+router.use("/clients", clientsRoutes)
 
 export default router
