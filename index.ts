e4202a8b5117ec2673e7db7f488e1946cc94d31e
Martín Gesualdo <mgesudev@gmail.com>
2023-06-19 20:48:26 -0300

    Cambios

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URL,
     methods: ["POST", "PUT", "DELETE", "GET", "OPTIONS"],
     credentials: true,
   })
