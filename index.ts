6cc0423b1162325922c36c22411340f42f194a96
Martín Gesualdo <mgesudev@gmail.com>
2023-06-19 20:33:55 -0300

    Cambios

--- index.ts
+++ index.ts
@@ -9,7 +9,13 @@ const app = express()
 connectDB()
 app.use(cookieParser())
 app.use(express.json())
-app.use(cors({ origin: "*", credentials: true }))
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["POST", "PUT", "DELETE", "GET", "OPTIONS"],
+    credentials: true,
+  })
+)
 app.use("/api", routes)
 
 const PORT = process.env.PORT || 4000
